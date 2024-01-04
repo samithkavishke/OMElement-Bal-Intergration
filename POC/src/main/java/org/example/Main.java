0a0ce74e50d576d56f3c7fb4d81bb2d1771fa424
samithkavishke <samithkarunathilake@email.com>
2024-01-04 14:52:08 +0530

    under developing

--- POC/src/main/java/org/example/Main.java
+++ POC/src/main/java/org/example/Main.java
@@ -1,15 +1,11 @@
 package org.example;
 
-import io.ballerina.runtime.api.values.BXml;
 import org.apache.axiom.om.OMAbstractFactory;
 import org.apache.axiom.om.OMElement;
 import org.apache.axiom.om.OMFactory;
 import org.apache.axiom.om.OMNamespace;
 
 public class Main {
-    private static void sain() {
-        BXml xml = null;
-    }
 
     public static void main(String[] args) {
         // Create an OMFactory
@@ -28,12 +24,16 @@ public class Main {
         OMElement childElement2 = factory.createOMElement("child2", namespace);
         childElement2.setText("World");
 
+        OMElement childElement3 = factory.createOMElement("child3", namespace);
+
+
         rootElement.addChild(childElement1);
         rootElement.addChild(childElement2);
 
-        System.out.println(rootElement);
-        // Print the XML representation of the root element
-        System.out.println(rootElement);
-        sain();
+        System.out.println(childElement1.getType());
+        System.out.println(rootElement.getType());
+
+//        System.out.println(rootElement);
     }
+
 }
