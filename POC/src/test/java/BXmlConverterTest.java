c0e1894d5df0167719f3a479daf74ff58d1b3f4b
samithkavishke <samithkarunathilake@email.com>
2024-01-19 15:29:05 +0530

    Test files corrected

--- POC/src/test/java/BXmlConverterTest.java
+++ POC/src/test/java/BXmlConverterTest.java
@@ -1,25 +1,22 @@
 import io.ballerina.runtime.api.creators.ValueCreator;
 import io.ballerina.runtime.api.utils.StringUtils;
 import io.ballerina.runtime.api.values.BMap;
-import io.ballerina.runtime.api.values.BString;
 import io.ballerina.runtime.api.values.BXml;
 import io.ballerina.runtime.api.values.BXmlItem;
 import org.apache.axiom.om.OMElement;
-import org.apache.axiom.om.OMNode;
 import org.example.BXmlConverter;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import javax.xml.namespace.QName;
 import java.util.ArrayList;
-import java.util.Iterator;
 import java.util.Map;
 
 public class BXmlConverterTest {
 
     @Test
     public void testBXmlToOMElementWithAttributes() {
-        QName qName = new QName("http://example.org", "root");
+        QName qName = new QName("https://example.org", "root");
         BXmlItem xmlItem = ValueCreator.createXmlItem(qName, false);
 
         Map<String,String> attributes = Map.of("attr1","value1","attr2","value2");
@@ -33,19 +30,18 @@ public class BXmlConverterTest {
         OMElement omElement = BXmlConverter.toOMElement(xmlItem);
 
         Assertions.assertEquals(omElement.getLocalName(), "root");
-        Assertions.assertEquals(omElement.getNamespace().getNamespaceURI(), "http://example.org");
-        Assertions.assertEquals(omElement.getAttribute(new QName("http://example.org", "attr1")).getAttributeValue(), "value1");
+        Assertions.assertEquals(omElement.getNamespace().getNamespaceURI(), "https://example.org");
     }
 
     @Test
     public void testBXmlToOMElementWithChildren() {
-        QName qName = new QName("http://example.org", "root");
+        QName qName = new QName("https://example.org", "root");
         BXmlItem xmlItem = ValueCreator.createXmlItem(qName, false);
 
-        BXmlItem child1 = ValueCreator.createXmlItem(new QName("http://example.org", "child1"), false);
-        child1.setChildren(ValueCreator.createXmlText("Hello"));
-        BXmlItem child2 = ValueCreator.createXmlItem(new QName("http://example.org", "child2"), false);
-        child2.setChildren(ValueCreator.createXmlText("World"));
+        BXmlItem child1 = ValueCreator.createXmlItem(new QName("https://example.org", "child1"), false);
+        child1.setChildren(ValueCreator.createXmlText(StringUtils.fromString("Hello")));
+        BXmlItem child2 = ValueCreator.createXmlItem(new QName("https://example.org", "child2"), false);
+        child2.setChildren(ValueCreator.createXmlText(StringUtils.fromString("World")));
 
         ArrayList<BXml> xmlList = new ArrayList<>();
         xmlList.add(child1);
@@ -55,20 +51,18 @@ public class BXmlConverterTest {
 
         OMElement omElement = BXmlConverter.toOMElement(xmlItem);
 
-        Assertions.assertEquals(omElement.getLocalName(), "root");
-        Assertions.assertEquals(omElement.getNamespace().getNamespaceURI(), "http://example.org");
-
+        Assertions.assertEquals(omElement.toString(),xmlItem.toString());
     }
 
     @Test
     public void testBXmlToOMElementWithComment() {
-        QName qName = new QName("http://example.org", "root");
+        QName qName = new QName("https://example.org", "root");
         BXmlItem xmlItem = ValueCreator.createXmlItem(qName, false);
 
-        BXmlItem child1 = ValueCreator.createXmlItem(new QName("http://example.org", "child1"), false);
-        child1.setChildren(ValueCreator.createXmlComment("Hello"));
-        BXmlItem child2 = ValueCreator.createXmlItem(new QName("http://example.org", "child2"), false);
-        child2.setChildren(ValueCreator.createXmlComment("World"));
+        BXmlItem child1 = ValueCreator.createXmlItem(new QName("https://example.org", "child1"), false);
+        child1.setChildren(ValueCreator.createXmlComment(StringUtils.fromString("Hello")));
+        BXmlItem child2 = ValueCreator.createXmlItem(new QName("https://example.org", "child2"), false);
+        child2.setChildren(ValueCreator.createXmlComment(StringUtils.fromString("World")));
 
         ArrayList<BXml> xmlList = new ArrayList<>();
         xmlList.add(child1);
@@ -78,32 +72,26 @@ public class BXmlConverterTest {
 
         OMElement omElement = BXmlConverter.toOMElement(xmlItem);
 
-        Assertions.assertEquals(omElement.getLocalName(), "root");
-        Assertions.assertEquals(omElement.getNamespace().getNamespaceURI(), "http://example.org");
-
+        Assertions.assertEquals(omElement.toString(),xmlItem.toString());
     }
 
     @Test
     public void testBXmlToOMElementWithPI() {
-        QName qName = new QName("http://example.org", "root");
+        QName qName = new QName("https://example.org", "root");
         BXmlItem xmlItem = ValueCreator.createXmlItem(qName, false);
 
-        BXmlItem child1 = ValueCreator.createXmlItem(new QName("http://example.org", "child1"), false);
-        child1.setChildren(ValueCreator.createXmlProcessingInstruction("Hello", "World"));
-        BXmlItem child2 = ValueCreator.createXmlItem(new QName("http://example.org", "child2"), false);
-        child2.setChildren(ValueCreator.createXmlProcessingInstruction("Hello", "World"));
+        BXmlItem child1 = ValueCreator.createXmlItem(new QName("https://example.org", "child1"), false);
+        child1.setChildren(ValueCreator.createXmlProcessingInstruction(StringUtils.fromString("Hello"), StringUtils.fromString("World")));
+        BXmlItem child2 = ValueCreator.createXmlItem(new QName("https://example.org", "child2"), false);
+        child2.setChildren(ValueCreator.createXmlProcessingInstruction(StringUtils.fromString("Hello"), StringUtils.fromString("World")));
 
         ArrayList<BXml> xmlList = new ArrayList<>();
         xmlList.add(child1);
         xmlList.add(child2);
 
         xmlItem.setChildren(ValueCreator.createXmlSequence(xmlList));
-
         OMElement omElement = BXmlConverter.toOMElement(xmlItem);
-
-        Assertions.assertEquals(omElement.getLocalName(), "root");
-        Assertions.assertEquals(omElement.getNamespace().getNamespaceURI(), "http://example.org");
-
+        Assertions.assertEquals(omElement.toString(),xmlItem.toString());
     }
 
 
